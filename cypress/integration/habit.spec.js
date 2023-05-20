0ae9713faeaaef79e294bcf5d3d3f5ba2773a019
Siegfried <siegfried.bozza@renuo.ch>
2023-05-20 14:54:50 +0200

    update e2e.yml

---
+++ cypress/integration/habit.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Habits dashboard', () => {
+
+    beforeEach(() => {
+        cy.visit('/habits')
+    })
+
+    describe('Habits List', () => {
+
+    })
+
+})
