009201d483de62c357399e68cb04dd52ecf755e6
Siegfried <siegfried.bozza@renuo.ch>
2023-05-20 16:50:27 +0200

    add tests to habit.spec

--- cypress/integration/habit.spec.js
+++ cypress/integration/habit.spec.js
@@ -23,19 +23,32 @@ describe('Habits dashboard', () => {
         it('should display habit card when new habit is added', () => {
             const habitInput= "Go to the gym"
             createHabit(habitInput)
+
             cy.contains(habitInput)
                 .should('be.visible')
                 .should("have.class", "HabitCard__habit-container")
         })
 
-        it('should toggle icon when clicked', () => {
-            const habitInput= "Go to the gym"
-            createHabit(habitInput)
-            cy.get("[src='/static/media/close.fa7e5ead.svg']")
+        it('should modal habit toggle icon when clicked', () => {
+            createHabit("Go to the gym")
+
+            const img01 = '/static/media/close.fa7e5ead.svg'
+            const img02 = '/static/media/check.9e8832df.svg'
+            cy.get(`[src='${img01}']`)
                 .should('be.visible')
                 .click()
-            cy.get("[src='/static/media/check.9e8832df.svg']")
+            cy.get(`[src='${img02}']`)
                 .should('be.visible')
+                .click()
+            cy.get(`[src='${img01}']`)
+                .should('be.visible')
+        })
+
+        it('should close modal when modal close btn is clicked', () => {
+            cy.get("#habit-add-btn").click()
+            cy.get("input[placeholder='Habit']").should('be.visible')
+            cy.contains('button', /close/i).click()
+            cy.contains('Habit Checklist').should('be.visible')
         })
     })
 })
